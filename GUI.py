f4e650ae5bc98b4545899dd2836b09ebbad9799a
olipfeifferthkoeln <92851939+olipfeifferthkoeln@users.noreply.github.com>
2022-01-26 18:35:17 +0100

    GUI automatisierung

--- GUI.py
+++ GUI.py
@@ -96,7 +96,7 @@ SimulationDict = {
 'simulationName' : 'next_2_sun',
 'simulationMode' : 2, 
 'localFile' : True, # Decide wether you want to use a  weather file or try to download one for the coordinates
-'weatherFile' : (rootPath +'/WeatherData/Aasen_Germany/wetterfile_2.csv'), #weather file in TMY format 
+'weatherFile' : (rootPath +'/WeatherData/Aasen_Germany/wetterfile2.csv'), #weather file in TMY format 
 'spectralReflectancefile' : (rootPath + '/ReflectivityData/grass_interpolated.csv'),
 'cumulativeSky' : False, # Mode for RayTracing: CumulativeSky or hourly
 'startHour' : (2019, 11, 1, 0),  # Only for hourly simulation, yy, mm, dd, hh
@@ -125,7 +125,7 @@ SimulationDict = {
 'longitude' : -105.172, 
 'latitude' : 39.739,
 'gcr' : 0.35, #ground coverage ratio (module area / land use)
-'module_type' : 'NREL row 2', #Name of Module
+'module_type' : 'Jollywood', #Name of Module
 }
 
 # is in Function StartSimulation()
