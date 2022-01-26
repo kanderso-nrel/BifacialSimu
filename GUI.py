38bf7e2e05a0229dfa8d87fa1d069afe20956461
Oliver Pfeiffer <oliver.pfeiffer@smail.th-koeln.degit config --global user.email oliver.pfeiffer@smail.th-koeln.de>
2022-01-26 17:40:47 +0100

    Upload Wetterfile

--- GUI.py
+++ GUI.py
@@ -93,30 +93,32 @@ import BifacialSimu_dataHandler
 
 # simulation parameters and variables
 SimulationDict = {
-'simulationName' : 'NREL_best_field_row_2',
-'simulationMode' : 1, 
+'simulationName' : 'next_2_sun',
+'simulationMode' : 2, 
 'localFile' : True, # Decide wether you want to use a  weather file or try to download one for the coordinates
 'weatherFile' : (rootPath +'/WeatherData/Golden_USA/SRRLWeatherdata Nov_Dez_2.csv'), #weather file in TMY format 
-'spectralReflectancefile' : (rootPath + '/ReflectivityData/interpolated_reflectivity.csv'),
+'spectralReflectancefile' : (rootPath + '/ReflectivityData/grass_interpolated.csv'),
 'cumulativeSky' : False, # Mode for RayTracing: CumulativeSky or hourly
-'startHour' : (2019, 11, 1, 0),  # Only for hourly simulation, yy, mm, dd, hh
-'endHour' : (2019, 11, 16, 0),  # Only for hourly simulation, yy, mm, dd, hh
-'utcOffset': -7,
-'tilt' : 10, #tilt of the PV surface [deg]
-'singleAxisTracking' : True, # singleAxisTracking or not
+'startHour' : (2021, 1, 1, 0),  # Only for hourly simulation, yy, mm, dd, hh
+'endHour' : (2021, 3, 1, 0),  # Only for hourly simulation, yy, mm, dd, hh
+'utcOffset': 1,
+'tilt' : 90, #tilt of the PV surface [deg]
+'singleAxisTracking' : False, # singleAxisTracking or not
 'backTracking' : False, # Solar backtracking is a tracking control program that aims to minimize PV panel-on-panel shading 
 'ElectricalMode_simple': False, # simple electrical Simulation after PVSyst, use if rear module parameters are missing
 'limitAngle' : 60, # limit Angle for singleAxisTracking
-'hub_height' : 1.3, # Height of the rotation axis of the tracker [m]
-'azimuth' : 180, #azimuth of the PV surface [deg] 90°: East, 135° : South-East, 180°:South
-'nModsx' : 1, #number of modules in x-axis
+'hub_height' : 2, # Height of the rotation axis of the tracker [m]
+'azimuth' : 90, #azimuth of the PV surface [deg] 90°: East, 135° : South-East, 180°:South
+'nModsx' : 20, #number of modules in x-axis
 'nModsy' : 1, #number of modules in y-axis
-'nRows' : 3, #number of rows
+'nRows' : 5, #number of rows
 'sensorsy' : 5, #number of sensors
 'moduley' : 2 ,#length of modules in y-axis
 'modulex' : 1, #length of modules in x-axis  
 'hourlyMeasuredAlbedo' : True, # True if measured albedo values in weather file
-'spectralAlbedo' : True, # Option to calculate a spectral Albedo 
+'spectralAlbedo' : False, # Option to calculate a spectral Albedo 
+'fixAlbedo' : False, #reaktion auf Error VF braucht offenbar diesen Wert in line 658 (radiation handler)
+'variableAlbedo': False, #reaktion auf Error VF braucht offenbar diesen Wert in line 663 (radiation handler)
 'albedo' : 0.26, # Measured Albedo average value, if hourly isn't available
 'frontReflect' : 0.03, #front surface reflectivity of PV rows
 'BackReflect' : 0.05, #back surface reflectivity of PV rows
